cece329f5eb674c17fdb39f4ffd4c477690b84be
shakti <shakti.dev46@gmail.com>
2022-03-05 07:50:22 +0530

    assignment day2

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+    <h1>Mobile Operating System</h1>
+   <ul>
+     <li>Android</li>
+     <li>Blckberry</li>
+     <li>iPhone</li>
+     <li>Windows Phone</li>
+   </ul>
+   <h1>Mobile Manufacturers</h1>
+   <ul id="shakti">
+     <li>Samsung</li>
+     <li>HTC</li>
+   </ul>
+   <ul>
+     <li>Micromax</li>
+     <ul id="shakti1">
+     <li>Apple</li>
+   </ul>
+   </ul>
+  
+  </div>
   );
 }
 
